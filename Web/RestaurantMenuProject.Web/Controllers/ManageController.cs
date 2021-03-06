9032772351cc585c4a96c14f6fc793a9e9fb2fd8
thunder913 <andon_00@abv.bg>
2021-03-06 20:30:06 +0200

    Added Manage UI (sort of) and Add/Remove controllers and some photos

---
+++ Web/RestaurantMenuProject.Web/Controllers/ManageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantMenuProject.Web.Controllers
+{
+    public class ManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string type)
+        {
+            return this.View();
+        }
+
+        public IActionResult Remove()
+        {
+            return this.View();
+        }
+    }
+}
