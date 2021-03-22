b568fb48d67a2b06b75888d160634cd65eb6d445
thunder913 <andon_00@abv.bg>
2021-03-22 11:16:23 +0200

    Added orders button in admin page and fixed minor bugs

---
+++ Data/RestaurantMenuProject.Data/Migrations/20210320133100_AddedPriceForOneInOrders.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantMenuProject.Data.Migrations
+{
+    public partial class AddedPriceForOneInOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PriceForOne",
+                table: "OrdersDishes",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PriceForOne",
+                table: "OrderDrinks",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceForOne",
+                table: "OrdersDishes");
+
+            migrationBuilder.DropColumn(
+                name: "PriceForOne",
+                table: "OrderDrinks");
+        }
+    }
+}
---
+++ Data/RestaurantMenuProject.Data/Seeding/AdminSeeder.cs
@@ -0,0 +1,35 @@
+﻿namespace RestaurantMenuProject.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+    using RestaurantMenuProject.Common;
+    using RestaurantMenuProject.Data.Models;
+
+    public class AdminSeeder : ISeeder
+    {
+        public async Task SeedAsync(
+            ApplicationDbContext dbContext,
+            IServiceProvider serviceProvider)
+        {
+            const string email = "admin@admin.com";
+            // TODO make it not upload to github
+            var user = dbContext.Users.FirstOrDefault(x => x.Email == email);
+            if (user == null)
+            {
+                var userToAdd = new ApplicationUser()
+                {
+                    Email = email,
+                    UserName = email,
+                };
+
+                var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                await userManager.CreateAsync(userToAdd, "123456");
+                await userManager.AddToRoleAsync(userToAdd, GlobalConstants.AdministratorRoleName);
+            }
+        }
+    }
+}
