00929b10f60b100713b249d6fca8b85ed12ad6d8
thunder913 <andon_00@abv.bg>
2021-04-15 12:29:52 +0300

    Added privacy page and fixed tests bugs

--- Tests/RestaurantMenuProject.Services.Data.Tests/AllergenServiceTests.cs
+++ Tests/RestaurantMenuProject.Services.Data.Tests/AllergenServiceTests.cs
@@ -1,63 +1,69 @@
 ﻿namespace RestaurantMenuProject.Services.Data.Tests
 {
-    using AutoMapper;
-    using DeepEqual.Syntax;
-    using Moq;
-    using RestaurantMenuProject.Data.Common.Repositories;
-    using RestaurantMenuProject.Data.Models;
-    using RestaurantMenuProject.Services.Mapping;
-    using RestaurantMenuProject.Web.ViewModels;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using System.Text;
     using System.Threading.Tasks;
+
+    using AutoMapper;
+    using DeepEqual.Syntax;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using RestaurantMenuProject.Data;
+    using RestaurantMenuProject.Data.Common.Repositories;
+    using RestaurantMenuProject.Data.Models;
+    using RestaurantMenuProject.Data.Repositories;
+    using RestaurantMenuProject.Services.Mapping;
+    using RestaurantMenuProject.Web.ViewModels;
     using Xunit;
 
-    public class AllergenServiceTests
+    public class AllergenServiceTests : IDisposable
     {
+        private readonly ApplicationDbContext dbContext;
+
+        private readonly IDeletableEntityRepository<Allergen> allergenRepository;
+
         public AllergenServiceTests()
         {
             AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("db").Options;
+            this.dbContext = new ApplicationDbContext(options);
+            this.allergenRepository = new EfDeletableEntityRepository<Allergen>(this.dbContext);
         }
-
-        private AllergenService AllergenService { get; set; }
-
-        private List<Allergen> Allergens { get; set; }
 
         [Fact]
         public async Task AddAllergenAsyncWorksCorrectly()
         {
-            this.SetupMoq();
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test" });
+            var allergenService = new AllergenService(this.allergenRepository);
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test" });
 
-            Assert.True(this.Allergens.Count == 1);
+            Assert.True(this.allergenRepository.All().Count() == 1);
 
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" });
-            Assert.True(this.Allergens.Count == 2);
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" });
+            Assert.True(this.allergenRepository.All().Count() == 2);
 
         }
 
         [Fact]
         public async Task AddAllergenAsyncThrowsExceptionIfNameAlreadyExists()
         {
-            this.SetupMoq();
-
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" });
-            await Assert.ThrowsAsync<Exception>(() => this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" }));
+            var allergenService = new AllergenService(this.allergenRepository);
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" });
+            await Assert.ThrowsAsync<Exception>(() => allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" }));
         }
 
         [Fact]
         public async Task GetAllergensWithIdWorksCorrectly()
         {
-            this.SetupMoq();
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test1" });
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" });
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test3" });
+            var allergenService = new AllergenService(this.allergenRepository);
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test1" });
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test2" });
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Name = "test3" });
 
-            var actual = this.AllergenService.GetAllergensWithId().ToList();
-            var expected = this.Allergens.Select(x => new AllergenViewModel(){ Id = x.Id, Name = x.Name }).ToList();
+            var actual = allergenService.GetAllergensWithId().ToList();
+            var expected = this.allergenRepository.All().Select(x => new AllergenViewModel(){ Id = x.Id, Name = x.Name }).ToList();
 
             expected.ShouldDeepEqual(actual);
         }
@@ -65,34 +71,27 @@
         [Fact]
         public async Task GetAllergensWithIdsShouldWorkCorrectly()
         {
-            this.SetupMoq();
-
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Id = 1, Name = "test1" });
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Id = 2, Name = "test2" });
-            await this.AllergenService.AddAllergenAsync(new AllergenViewModel() { Id = 3, Name = "test3" });
+            var allergenService = new AllergenService(this.allergenRepository);
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Id = 1, Name = "test1" });
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Id = 2, Name = "test2" });
+            await allergenService.AddAllergenAsync(new AllergenViewModel() { Id = 3, Name = "test3" });
 
             var ids = new List<int>() { 1, 2 };
-            var actual = this.AllergenService.GetAllergensWithIds(ids);
-            var expected = this.Allergens.Where(x => ids.Contains(x.Id));
+            var actual = allergenService.GetAllergensWithIds(ids);
+            var expected = this.allergenRepository.All().Where(x => ids.Contains(x.Id));
 
             expected.ShouldDeepEqual(actual);
 
             ids = new List<int>() { 3 };
-            actual = this.AllergenService.GetAllergensWithIds(ids);
-            expected = this.Allergens.Where(x => ids.Contains(x.Id));
+            actual = allergenService.GetAllergensWithIds(ids);
+            expected = this.allergenRepository.All().Where(x => ids.Contains(x.Id));
 
             expected.ShouldDeepEqual(actual);
         }
 
-        private void SetupMoq()
+        public void Dispose()
         {
-            this.Allergens = new List<Allergen>();
-            var allergenRepo = new Mock<IDeletableEntityRepository<Allergen>>();
-            allergenRepo.Setup(x => x.AddAsync(It.IsAny<Allergen>())).Callback((Allergen all) => this.Allergens.Add(all));
-            allergenRepo.Setup(x => x.All()).Returns(this.Allergens.AsQueryable());
-            allergenRepo.Setup(x => x.AllAsNoTracking()).Returns(this.Allergens.AsQueryable);
-            this.AllergenService = new AllergenService(allergenRepo.Object);
+            this.dbContext.Database.EnsureDeleted();
         }
-
     }
 }
