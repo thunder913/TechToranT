1a7eed687f3bf3e860c8050bb22f8a69ae6ee948
thunder913 <andon_00@abv.bg>
2021-08-04 15:30:22 +0300

    added some unit tests

---
+++ Tests/RestaurantMenuProject.Services.Data.Tests/UserDislikeServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestaurantMenuProject.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantMenuProject.Services.Data.Tests
+{
+    public class UserDislikeServiceTests : BaseServiceTests
+    {
+        private IUserDislikeService UserDislikeService => this.ServiceProvider.GetRequiredService<IUserDislikeService>();
+
+        [Fact]
+        public async Task AddDislikeToCommentAsyncWorksCorrectly()
+        {
+            await this.PopulateDB();
+            var commentId = 1;
+            var userId = "user1";
+
+            await this.UserDislikeService.AddDislikeToCommentAsync(userId, commentId);
+
+            var dislike = this.DbContext.UsersDislikes.FirstOrDefault();
+
+            Assert.Equal(userId, dislike.UserId);
+            Assert.Equal(commentId, dislike.CommentId);
+        }
+
+        [Fact]
+        public async Task HasUserDislikedACommentWorksCorrectly()
+        {
+            await this.PopulateDB();
+            var userId = "user1";
+            var commentId = 1;
+
+            await this.UserDislikeService.AddDislikeToCommentAsync(userId, commentId);
+
+            var commentOne = this.UserDislikeService.HasUserDislikedAComment(userId, commentId);
+            var commentTwo = this.UserDislikeService.HasUserDislikedAComment(userId, 2);
+
+            Assert.True(commentOne);
+            Assert.False(commentTwo);
+        }
+
+        [Fact]
+        public async Task RemoveDislikeAsyncWorksCorrectly()
+        {
+            await this.PopulateDB();
+            var userId = "user1";
+            var commentId = 1;
+
+            await this.UserDislikeService.AddDislikeToCommentAsync(userId, commentId);
+            await this.UserDislikeService.RemoveDislikeAsync(userId, commentId);
+
+            var dislike = this.DbContext.UsersDislikes.FirstOrDefault();
+            Assert.Null(dislike);
+        }
+
+        [Fact]
+        public async Task RemoveDislikeAsyncThrowsWhenGivenInvalidData()
+        {
+            await this.PopulateDB();
+            var userId = "user1";
+            var commentId = 1;
+
+            await this.UserDislikeService.AddDislikeToCommentAsync(userId, commentId);
+            await Assert.ThrowsAsync<Exception>(async () => await this.UserDislikeService.RemoveDislikeAsync(userId + "invalid", commentId));
+            await Assert.ThrowsAsync<Exception>(async () => await this.UserDislikeService.RemoveDislikeAsync(userId, commentId + 99));
+            await Assert.ThrowsAsync<Exception>(async () => await this.UserDislikeService.RemoveDislikeAsync("user2", 3));
+        }
+    }
+}
---
+++ Tests/RestaurantMenuProject.Services.Data.Tests/UserLikeServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestaurantMenuProject.Services.Data.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RestaurantMenuProject.Services.Data.Tests
+{
+    public class UserLikeServiceTests : BaseServiceTests
+    {
+        private IUserLikeService UserLikeService => this.ServiceProvider.GetRequiredService<IUserLikeService>();
+
+        [Fact]
+        public async Task AddLikeToCommentAsyncWorksCorrectly()
+        {
+            await this.PopulateDB();
+            var commentId = 1;
+            var userId = "user1";
+
+            await this.UserLikeService.AddLikeToCommentAsync(userId, commentId);
+
+            var like = this.DbContext.UsersLikes.FirstOrDefault();
+
+            Assert.Equal(userId, like.UserId);
+            Assert.Equal(commentId, like.CommentId);
+        }
+
+        [Fact]
+        public async Task HasUserLikedACommentWorksCorrectly()
+        {
+            await this.PopulateDB();
+            var userId = "user1";
+            var commentId = 1;
+
+            await this.UserLikeService.AddLikeToCommentAsync(userId, commentId);
+
+            var commentOne = this.UserLikeService.HasUserLikedAComment(userId, commentId);
+            var commentTwo = this.UserLikeService.HasUserLikedAComment(userId, 2);
+
+            Assert.True(commentOne);
+            Assert.False(commentTwo);
+        }
+
+        [Fact]
+        public async Task RemoveLikeAsyncWorksCorrectly()
+        {
+            await this.PopulateDB();
+            var userId = "user1";
+            var commentId = 1;
+
+            await this.UserLikeService.AddLikeToCommentAsync(userId, commentId);
+            await this.UserLikeService.RemoveLikeAsync(userId, commentId);
+
+            var like = this.DbContext.UsersLikes.FirstOrDefault();
+            Assert.Null(like);
+        }
+
+        [Fact]
+        public async Task RemoveLikeAsyncThrowsWhenGivenInvalidData()
+        {
+            await this.PopulateDB();
+            var userId = "user1";
+            var commentId = 1;
+
+            await this.UserLikeService.AddLikeToCommentAsync(userId, commentId);
+            await Assert.ThrowsAsync<Exception>(async () => await this.UserLikeService.RemoveLikeAsync(userId + "invalid", commentId));
+            await Assert.ThrowsAsync<Exception>(async () => await this.UserLikeService.RemoveLikeAsync(userId, commentId + 99));
+            await Assert.ThrowsAsync<Exception>(async () => await this.UserLikeService.RemoveLikeAsync("user2", 3));
+        }
+    }
+}
